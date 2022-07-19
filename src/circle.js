6e63185279abc202c36e2980639b4b6288d8273b
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-19 18:43:01 -0300

    4 resolvido

--- src/circle.js
+++ src/circle.js
@@ -25,9 +25,11 @@ const circle = (radius) => {
 
   return {
     radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
+    area: parseFloat((PI * radius * radius).toPrecision(2)),
+    circumference: parseFloat((2 * PI * radius).toPrecision(2)),
   };
 };
+
+console.log(circle(2));
 
 module.exports = circle;
